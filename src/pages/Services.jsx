4f5e5a9ab50df227ad86a22d4a846cb68702249e
31pablo05 <Pabloproboste64@gmail.com>
2025-05-22 09:30:49 -0300

    video en service

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,7 +4,27 @@ import ListaServicios from "../components/service/ListaServicios";
 const Services = () => {
   return (
     <div>
+      {/* Lista de servicios */}
       <ListaServicios />
+
+   {/* Video decorativo responsive */}
+<div className="relative w-full overflow-hidden mt-10 flex items-center justify-center bg-black 
+                max-h-[80vh] md:max-h-none md:h-[600px]">
+
+  <video
+    className="w-full h-full object-contain md:object-cover md:w-auto md:h-full"
+    autoPlay
+    loop
+    muted
+    playsInline
+  >
+    <source src="/assets/videos/ReservaTurno.mp4" type="video/mp4" />
+    Tu navegador no soporta el video HTML5.
+  </video>
+
+  
+</div>
+
     </div>
   );
 };
