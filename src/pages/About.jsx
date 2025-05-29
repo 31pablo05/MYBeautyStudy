35f68f73fcb9b9c6fdc788f3c7556a7783e33349
31pablo05 <Pabloproboste64@gmail.com>
2025-05-29 11:47:16 -0300

    ajuste index.html helmet mejoras para seo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Presentacion from "../components/about/Presentacion";
 import Historia from "../components/about/Historia";
 import MisionVision from "../components/about/MisionVision";
 
 const About = () => {
   return (
-    <div>
-      <Presentacion />
-      <Historia />
-      <MisionVision />
-    </div>
+    <>
+      <Helmet>
+        <title>Sobre Nosotros | MyBeautyStudy</title>
+        <meta
+          name="description"
+          content="Conocé la historia, misión y visión de MyBeautyStudy, especialistas en belleza facial en Trelew. Profesionalismo, compromiso y pasión por resaltar tu belleza natural."
+        />
+        <meta
+          name="keywords"
+          content="sobre nosotros, historia, misión, visión, estética, cejas, pestañas, MyBeautyStudy, Trelew"
+        />
+      </Helmet>
+
+      <div>
+        <Presentacion />
+        <Historia />
+        <MisionVision />
+      </div>
+    </>
   );
 };
 
